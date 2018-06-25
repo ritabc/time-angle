749074cd70488849114e5cbd7aaf292bee157adf
Rita Bennett-Chew and Will Swanson <rita@>
2018-06-25 16:29:09 -0700

    add passing test and functionality working

--- lib/time_angle.rb
+++ lib/time_angle.rb
@@ -3,6 +3,8 @@
      false
    elsif
      time_array[0] > 0 && time_array[1] > 0
-     true
+     x = (time_array[0]*30)
+     y = (time_array[1]*6)
+     (x - y).abs
    end
  end
--- spec/time_angle_spec.rb
+++ spec/time_angle_spec.rb
@@ -5,7 +5,17 @@ describe('#time_angle') do
   it('validates that two intergers were input') do
     expect(time_angle(["string",30])).to(eq(false))
   end
-  it('validates that two intergers were input') do
-    expect(time_angle([1,30])).to(eq(true))
+  # these two tests were invalidated by subsequent functionality
+  # butthe did work when we initially implemented them
+  # and the fact that the subesquent functionality works
+  # proves that criteria for these are still being validated 
+  # it('validates that two intergers were input') do
+  #   expect(time_angle([1,30])).to(eq(true))
+  # end
+  # it('translates two numbers into degrees') do
+  #   expect(time_angle([2,20])).to(eq([60,120]))
+  # end
+  it('subtracts angle 1 from angle 2, returns absolute value') do
+    expect(time_angle([2,20])).to(eq(60))
   end
 end
